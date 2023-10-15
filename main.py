3edfb47f0747f12b9d0a74501c4e26b424fa4832
Aldin Jusufagic <90244948+AldinTheLegend@users.noreply.github.com>
2023-10-15 14:12:16 +0200

    added listbox

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import tkinter as tk
 import tempfile, base64, zlib
 
-
-root = tk.Tk()
-ICON = zlib.decompress(base64.b64decode('eJxjYGAEQgEBBiDJwZDBy'
-    'sAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc='))
+class MyGUI:
+    
+    def __init__(self):
+        root = tk.Tk()
+        ICON = zlib.decompress(base64.b64decode('eJxjYGAEQgEBBiDJwZDBy'
+            'sAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc='))
  
-_, ICON_PATH = tempfile.mkstemp()
-with open(ICON_PATH, 'wb') as icon_file:
-    icon_file.write(ICON) #kod jag snodde från python forum
+        _, ICON_PATH = tempfile.mkstemp()
+        with open(ICON_PATH, 'wb') as icon_file:
+            icon_file.write(ICON) #kod jag snodde från python forum
 
-root.title("")
-root.iconbitmap(default=ICON_PATH)
-root.overrideredirect(True)
+        root.title("")
+        root.iconbitmap(default=ICON_PATH)
+        root.overrideredirect(True)
 
-root.geometry()
+        Lb = tk.Listbox(master=root)
+        options = ["option1", "option2", "option3"]
+        for i in range(len(options)):
+            Lb.insert(i, options[i])
+        Lb.pack()
 
-root.mainloop()
+        root.mainloop()
+
+MyGUI()
+
