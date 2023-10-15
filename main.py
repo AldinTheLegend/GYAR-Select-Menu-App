7f75c013bb0660d08d3c1da095e11daa1fa0531a
AldinTheLegend <aldin.jusufagic@elev.ga.lbs.se>
2023-10-15 14:39:45 +0100

    läser filer från directory

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import tkinter as tk
 import tempfile, base64, zlib
+import os
 
+path = "/home/raspberry/Pictures"
 class MyGUI:
-    
     def __init__(self):
         root = tk.Tk()
         root.title("")
         root.overrideredirect(True)
 
         Lb = tk.Listbox(master=root)
-        options = ["option1", "option2", "option3"]
+        options = os.listdir(path)
         for i in range(len(options)):
             Lb.insert(i, options[i])
         Lb.pack()
 
         root.mainloop()
-
 MyGUI()
 
