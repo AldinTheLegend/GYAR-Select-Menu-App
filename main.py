ba976d6d500fa7b2ea0d1ed4a90fe891bfa108f9
AldinTheLegend <aldin.jusufagic@elev.ga.lbs.se>
2023-10-15 13:34:40 +0100

    tog bort onödig kod

--- main.py
+++ main.py
@@ -5,15 +5,7 @@ class MyGUI:
     
     def __init__(self):
         root = tk.Tk()
-        ICON = zlib.decompress(base64.b64decode('eJxjYGAEQgEBBiDJwZDBy'
-            'sAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc='))
- 
-        _, ICON_PATH = tempfile.mkstemp()
-        with open(ICON_PATH, 'wb') as icon_file:
-            icon_file.write(ICON) #kod jag snodde från python forum
-
         root.title("")
-        root.iconbitmap(default=ICON_PATH)
         root.overrideredirect(True)
 
         Lb = tk.Listbox(master=root)
