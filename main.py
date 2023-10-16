6ae9ee93215f49f8cace2b240de070701c147294
Aldin Jusufagic <90244948+AldinTheLegend@users.noreply.github.com>
2023-10-16 13:53:18 +0200

    windows things

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import tkinter as tk
 import tempfile, base64, zlib
 import os
 
-path = "/home/raspberry/Pictures"
+path = "C:\Users\aldin.jusufagic\Pictures\Saved Pictures" #/home/raspberry/Pictures
 class MyGUI:
     def __init__(self):
         root = tk.Tk()
         root.title("")
         root.overrideredirect(True)
-
+        
         Lb = tk.Listbox(master=root)
         options = os.listdir(path)
         for i in range(len(options)):
