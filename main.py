3243c57907bbca9ea8724ace2a5f03b542f9d9e2
Aldin Jusufagic <90244948+AldinTheLegend@users.noreply.github.com>
2023-10-16 15:03:19 +0200

    fixed windows file path and removed border

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import tkinter as tk
 import tempfile, base64, zlib
 import os
 
-path = "C:\Users\aldin.jusufagic\Pictures\Saved Pictures" #/home/raspberry/Pictures
+path = "C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures" #/home/raspberry/Pictures
 class MyGUI:
     def __init__(self):
         root = tk.Tk()
         root.title("")
         root.overrideredirect(True)
         
-        Lb = tk.Listbox(master=root)
+        Lb = tk.Listbox(master=root, bd=0)
         options = os.listdir(path)
         for i in range(len(options)):
             Lb.insert(i, options[i])
