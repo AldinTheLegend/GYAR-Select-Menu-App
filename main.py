fa3a3ab90e208deb8c218b160ec237bd2a66f7d5
AldinTheLegend <aldin.jusufagic@elev.ga.lbs.se>
2023-10-18 17:17:43 +0100

    Wait with font

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import tkinter as tk 
+import tkinter as tk
 from tkinter import *
 import os
+from PIL import Image, ImageFont, ImageDraw
 
-path = "C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures" #/home/raspberry/Pictures
+path = "/home/raspberry/Pictures" #C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures
 
 class MyGUI:
     def __init__(self):
         root = tk.Tk()
         root.title("")
         root.overrideredirect(True)
-        
-        font_path = "avenir-heavy_QYJA9/Avenir Heavy.ttf"
-        custom_font = tk.font.Font(family="avenir", size=12, file=font_path)
 
-        Lb = tk.Listbox(master=root, bd=0, font=custom_font)
+        #font_path = os.getcwd() + "/avenir-heavy_QYJA9/Avenir Heavy.ttf"
+        
+        Lb = tk.Listbox(master=root, bd=0)
         options = os.listdir(path)
         for i in range(len(options)):
             Lb.insert(i, options[i])
         Lb.pack()
-
+        
         root.mainloop()
 MyGUI()
 
