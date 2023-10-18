399aeda40dfde918e36e372addc9f39c05e4eba8
Aldin Jusufagic <90244948+AldinTheLegend@users.noreply.github.com>
2023-10-18 17:31:02 +0200

    font

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import tkinter as tk
-import tempfile, base64, zlib
+import tkinter as tk 
+from tkinter import *
 import os
 
 path = "C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures" #/home/raspberry/Pictures
+
 class MyGUI:
     def __init__(self):
         root = tk.Tk()
         root.title("")
         root.overrideredirect(True)
         
-        Lb = tk.Listbox(master=root, bd=0)
+        font_path = "avenir-heavy_QYJA9/Avenir Heavy.ttf"
+        custom_font = tk.font.Font(family="avenir", size=12, file=font_path)
+
+        Lb = tk.Listbox(master=root, bd=0, font=custom_font)
         options = os.listdir(path)
         for i in range(len(options)):
             Lb.insert(i, options[i])
