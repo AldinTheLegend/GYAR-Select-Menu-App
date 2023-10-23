57f78f069cb7ec6b3e4b7a6dc3ce8fecdded0095
AldinTheLegend <aldin.jusufagic@elev.ga.lbs.se>
2023-10-23 20:57:18 +0100

    kan printa output från listbox

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ from tkinter import *
 import os
 from PIL import Image, ImageFont, ImageDraw
 
-path = "/home/raspberry/Pictures" #C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures
+pathToGames = "/home/raspberry/Pictures" #C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures
+pathToEmulator = "/snap/bin/visualboyadvance-m"
+
+
+def callBack(event):
+    print(event.widget.curselection())
+    #selection = lb.curselection()
+    #print(selection)
 
 class MyGUI:
     def __init__(self):
-        root = tk.Tk()
-        root.title("")
-        root.overrideredirect(True)
-        root.eval('tk::PlaceWindow . center')
+
+         root = tk.Tk()
+#        root.configure(bg='')
+         root.title("")
+         root.overrideredirect(True)
+         root.eval('tk::PlaceWindow . center')
 
         #font_path = os.getcwd() + "/avenir-heavy_QYJA9/Avenir Heavy.ttf"
-        
-        Lb = tk.Listbox(master=root, bd=0)
-        options = os.listdir(path)
-        for i in range(len(options)):
-            Lb.insert(i, options[i])
-        Lb.pack()
-        
-        root.mainloop()
+         games = []
+         options = os.listdir(pathToGames)
+         for i in range(len(options)):
+             games.insert(i, options[i])
+         gamelist = tk.Variable(value=games)
+
+         lb = tk.Listbox(master=root, listvariable=gamelist, selectmode='single', bd=0, fg='#222222')
+         lb.pack()
+         lb.bind('<<ListboxSelect>>', callBack)
+
+         root.mainloop()
+
 MyGUI()
 
