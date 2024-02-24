a1f4db4ae618ea8fdec59eba809b47e44b41101c
Aldin Jusufagic <90244948+AldinTheLegend@users.noreply.github.com>
2024-02-24 20:11:03 +0100

    reformat + added images to buttons

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import *
 import os
 import subprocess
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
 
 #pathToGames = "/home/raspberry/Downloads/ROMGames" #C:\\Users\\aldin.jusufagic\\Pictures\\Saved Pictures
 #pathToEmulator = "/snap/bin/visualboyadvance-m"
@@ -16,75 +19,104 @@ import subprocess
 #    selectedItem = event.widget.get(index)
 #    command = [pathToEmulator, "--fullscreen", selectedItem, "2/dev/null"]
 #    subprocess.call(command)
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-
         self.title("Ga,eList")
-        self.wm_attributes('-transparentcolor', 'red') #allt som är rött blir genomskinligt
-        self.config(background='red')
         self.initialize()
 
     def initialize(self):
         height = 270
         width = 308
-
+        #self.overrideredirect(True)
         self.center_window(width=width, height=height)
 
-        content = tk.Frame(self, background='red')
-        gamesIcon = tk.Button(content, text='Games', height=8, width=20, command=lambda: self.clear(windowIndex=1))
-        webbIcon = tk.Button(content, text= 'Webb', height=8, width=20)
-        filesIcon = tk.Button(content, text="Files", height=8, width=20)
-        settingsIcon = tk.Button(content, text='Settings', height=8, width=20)
+        background_path = 'bakcground.png'
 
-        content.columnconfigure(0, pad=4)
-        content.columnconfigure(1, pad=4)
-        content.rowconfigure(0, pad=4)
-        content.rowconfigure(1, pad=4) #lägger avstånd mellan knapparna
+        original_img = Image.open(background_path)
+        self.image = ImageTk.PhotoImage(original_img)
+    
+        background_label = tk.Label(self, image=self.image)
 
-        content.grid(column=0, row=0, columnspan=2, rowspan=2)
+        #content = tk.Frame(self)
+        gamesIcon = tk.Button(background_label, text='Games', height=8, width=20, command=lambda: self.clear(windowIndex=2))
+        webbIcon = tk.Button(background_label, text= 'Webb', height=8, width=20)
+        filesIcon = tk.Button(background_label, text="Files", height=8, width=20)
+        settingsIcon = tk.Button(background_label, text='Settings', height=8, width=20)
+
+        background_label.columnconfigure(0, pad=4)
+        background_label.columnconfigure(1, pad=4)
+        background_label.rowconfigure(0, pad=4)
+        background_label.rowconfigure(1, pad=4) #lägger avstånd mellan knapparna
+
+        background_label.grid(column=0, row=0, columnspan=2, rowspan=2)
         gamesIcon.grid(column=0, row=0)
         webbIcon.grid(column=1, row=0)
         filesIcon.grid(column=0, row=1)
-        settingsIcon.grid(column=1, row=1)
+        settingsIcon.grid(column=1, row=1) #placering av allt
         
     def clear(self, windowIndex):
         for widget in self.winfo_children():
             widget.destroy()
 
         if windowIndex == 1:
-            self.openGameMenu()
-        elif windowIndex == 2:
             self.initialize()
+        elif windowIndex == 2:
+            self.openGameMenu()
 
     def openGameMenu(self):
         self.geometry("1920x1080")
-        height = 1080
-        width = 1920
-        self.center_window(width=width, height=height)
+        self.center_window(1920, 1080)
+        
+        self.addBackgroundToGamesWindow()
+        
+    def addBackgroundToGamesWindow(self):
+        background_path = 'bakcground.png'
 
-        frame = tk.Frame(master=self, background='green', width=self.winfo_width(), height=self.winfo_height())
-        j = 0
-        #for column in frame.grid_info():
-            #j += 1
-            #frame.columnconfigure(j, pad=4)
+        original_img = Image.open(background_path)
+        self.image = ImageTk.PhotoImage(original_img)
+    
+        background_label = tk.Label(self, image=self.image)
+        background_label.pack(fill="both", expand=True)
+        self.addButtonsToBackground(background_label=background_label)
 
-        frame.rowconfigure(0, pad=4)
-        frame.rowconfigure(1, pad=4)
+    def addButtonsToBackground(self, background_label):
+        games = ["Pokemon FireRed", "Super Mario Bros.", "Pokemon LeafGreen", "Tetris", "The Legend Of Zelda", "sex", "sju"]
 
-        frame.grid(column=0, row=0)
-
-
-        games = ["apple", "orange", "banana", "m", "sdad", " asda", "taerf", "bitch", "apple2", "orange2", "banana2", "m2", "sdad2", " asda2"]
-
-        rows = -(-len(games) // 7)  # Ceiling division to get the number of rows
-        columns=7  # Maximum 7 columns
-
+        rows = 3  # Ceiling division to get the number of rows
+        columns = 7
+        for i in range(columns):
+            background_label.columnconfigure(i, weight=1, minsize=0)
+        for i in range(rows):
+            background_label.rowconfigure(i, weight=1, minsize=0)
+    
         for i, game in enumerate(games):
-            print(i)
-            button = tk.Button(frame, text=game, height=8, width=20)
-            row, col = divmod(i, columns)
-            button.grid(row=row, column=col, )#padx=5, pady=5)
+            row, col = divmod(i, columns)  # räknar ut current row och column
+    
+            button_frame = tk.Frame(background_label, background="#222222")
+            button_frame.grid(row=row, column=col, padx=10, pady=10)  # skapa frames för knappar
+
+            #gameArt = ImageTk.PhotoImage(self.getGameArtWork(game=game))
+            self.pic = self.getGameArtWork()
+
+            button = tk.Button(button_frame, height=180, width=200, image=self.pic, relief=RIDGE, bd=5)
+            button.image = self.pic  
+            button.pack()
+    
+            game_label = tk.Label(button_frame, text=game, background="#222222", foreground="white")  # skapa text under knappar
+            game_label.pack()
+
+    def getGameArtWork(self):
+
+        rqst = requests.get('https://cataas.com/cat')
+        imgdata = rqst.content
+
+        originalImage = Image.open(BytesIO(imgdata))
+
+        self.pic = ImageTk.PhotoImage(originalImage)
+        return self.pic
+
 
     def center_window(self, width, height):
         screen_width = self.winfo_screenwidth()
